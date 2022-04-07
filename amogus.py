43ae28036078b8b93161722fec3c0f13720aae9a
Nathan Relyea <nathan.r.relyea@gmail.com>
2022-04-06 23:39:53 -0700

    Finished Counts and Visualizations

--- amogus.py
+++ amogus.py
@@ -1,6 +1,10 @@
-from pickletools import string1
 import cv2
 import os
+from PIL import Image
+
+
+######     Scanning Amogi      ######
+
 
 def Color(image,pixel):
     return [image[pixel[1]][pixel[0]][0], image[pixel[1]][pixel[0]][1], image[pixel[1]][pixel[0]][2]]
@@ -143,15 +147,23 @@ height, width, _ = source.shape
 
 count = 0
 
+LeftAmogi = []
+RightAmogi = []
+
 for y in range(0,height-4):
     for x in range(0, width-3):
         pixel = (x,y)
         if CheckAmogusLeft(source,pixel) == "AMOGUS":
             print("Left ", pixel)
             count += 1
+            LeftAmogi.append(pixel)
         elif CheckAmogusRight(source,pixel) == "AMOGUS":
             print("Right ", pixel) 
             count += 1
+            RightAmogi.append(pixel)
+
+SmolLeftAmogi = []
+SmolRightAmogi = []
 
 for y in range(0,height-3):
     for x in range(0, width-3):
@@ -159,11 +171,117 @@ for y in range(0,height-3):
         if CheckSmolAmogusLeft(source,pixel) == "AMOGUS":
             print("Smol Left ", pixel)
             count += 1
+            SmolLeftAmogi.append(pixel)
         elif CheckSmolAmogusRight(source,pixel) == "AMOGUS":
             print("Smol Right ", pixel)
             count += 1
+            SmolRightAmogi.append(pixel)
 
 print("Total Amogi: ", count)
+
+
+######    Visualizing Amogi    ######
+
+
+def DrawLeftAmogus(image,pixel):
+    image.putpixel(pixel,(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,4),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,4),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,1),(255,255,255))
+    image.putpixel(NearbyPixel(pixel,1,1),(255,255,255)) 
+
+def DrawRightAmogus(image,pixel):
+    image.putpixel(NearbyPixel(pixel,3,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,4),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,4),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,1),(255,255,255))
+    image.putpixel(NearbyPixel(pixel,2,1),(255,255,255)) 
+
+def DrawSmolLeftAmogus(image,pixel):
+    image.putpixel(pixel,(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,1),(255,255,255))
+    image.putpixel(NearbyPixel(pixel,1,1),(255,255,255)) 
+
+def DrawSmolRightAmogus(image,pixel):
+    image.putpixel(NearbyPixel(pixel,3,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,0),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,1),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,2,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,0,2),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,1,3),(255,0,0))
+    image.putpixel(NearbyPixel(pixel,3,1),(255,255,255))
+    image.putpixel(NearbyPixel(pixel,2,1),(255,255,255)) 
+
+img = Image.new('RGB', (width, height))
+
+def IncrementDrawCount(drawCount,count):
+    drawCount += 1
+    print(str(drawCount) + "/" + str(count) + " drawn")
+    return drawCount
+
+drawCount = 0
+
+for a in range(0,len(LeftAmogi)):
+    DrawLeftAmogus(img,LeftAmogi[a])
+    drawCount = IncrementDrawCount(drawCount,count)
+
+for a in range(0,len(RightAmogi)):
+    DrawRightAmogus(img,RightAmogi[a])
+    drawCount = IncrementDrawCount(drawCount,count)
+
+for a in range(0,len(SmolLeftAmogi)):
+    DrawSmolLeftAmogus(img,SmolLeftAmogi[a])
+    drawCount = IncrementDrawCount(drawCount,count)
+
+for a in range(0,len(SmolRightAmogi)):
+    DrawSmolRightAmogus(img,SmolRightAmogi[a])
+    drawCount = IncrementDrawCount(drawCount,count)
+    
+
+
+
+
+expName = os.path.dirname(os.path.realpath(__file__))+'\\'+str(width)+"x"+str(height)+'_amogi.png'
+img.save(expName)
+img.show()
+
 
 
  
