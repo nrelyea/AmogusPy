3af59a66d77e7384ed19b8f1f2c6fe74799e8daa
Nathan Relyea <nathan.r.relyea@gmail.com>
2022-04-06 22:09:40 -0700

    All Amogi parsed
    1615 kekw

--- amogus.py
+++ amogus.py
@@ -33,21 +33,106 @@ def CheckAmogusLeft(image, pixel):
         and
         Color(image,pixel) != Color(image,NearbyPixel(pixel,3,0))       # behind head diff color
         and
-        Color(image,pixel) != Color(image,NearbyPixel(pixel,3,4))       # behind leg diff color
-        and
-        Color(image,pixel) != Color(image,NearbyPixel(pixel,3,4))       # behind foot diff color
+        Color(image,pixel) != Color(image,NearbyPixel(pixel,3,3))       # behind leg diff color
+        #and
+        #Color(image,pixel) != Color(image,NearbyPixel(pixel,3,4))       # behind foot diff color
     ):
         return "AMOGUS"
     else:
         return "NOPE"
     
+def CheckAmogusRight(image, pixel):
+    if(
+        Color(image,NearbyPixel(pixel,1,0)) == 
+        Color(image,NearbyPixel(pixel,2,0)) == 
+        Color(image,NearbyPixel(pixel,3,0)) ==
+        Color(image,NearbyPixel(pixel,0,1)) ==
+        Color(image,NearbyPixel(pixel,1,1)) ==
+        Color(image,NearbyPixel(pixel,0,2)) ==
+        Color(image,NearbyPixel(pixel,1,2)) ==
+        Color(image,NearbyPixel(pixel,2,2)) ==
+        Color(image,NearbyPixel(pixel,3,2)) ==
+        Color(image,NearbyPixel(pixel,1,3)) ==
+        Color(image,NearbyPixel(pixel,2,3)) ==
+        Color(image,NearbyPixel(pixel,3,3)) ==
+        Color(image,NearbyPixel(pixel,1,4)) ==
+        Color(image,NearbyPixel(pixel,3,4))
+        and
+        Color(image,NearbyPixel(pixel,2,1)) == Color(image,NearbyPixel(pixel,3,1))  # eyes same color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,2,1))       # eyes diff color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,2,4))       # between legs diff color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,0,0))       # behind head diff color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,0,3))       # behind leg diff color
+        #and
+        #Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,0,4))       # behind foot diff color
+    ):
+        return "AMOGUS"
+    else:
+        return "NOPE"
 
+def CheckSmolAmogusLeft(image, pixel):
+    if(
+        Color(image,pixel) == 
+        Color(image,NearbyPixel(pixel,1,0)) == 
+        Color(image,NearbyPixel(pixel,2,0)) ==
+        Color(image,NearbyPixel(pixel,2,1)) ==
+        Color(image,NearbyPixel(pixel,3,1)) ==
+        Color(image,NearbyPixel(pixel,0,2)) ==
+        Color(image,NearbyPixel(pixel,1,2)) ==
+        Color(image,NearbyPixel(pixel,2,2)) ==
+        Color(image,NearbyPixel(pixel,3,2)) ==
+        Color(image,NearbyPixel(pixel,0,3)) ==
+        Color(image,NearbyPixel(pixel,2,3))
+        and
+        Color(image,NearbyPixel(pixel,0,1)) == Color(image,NearbyPixel(pixel,1,1))  # eyes same color
+        and
+        Color(image,pixel) != Color(image,NearbyPixel(pixel,1,1))       # eyes diff color
+        and
+        Color(image,pixel) != Color(image,NearbyPixel(pixel,1,3))       # between legs diff color
+        and
+        Color(image,pixel) != Color(image,NearbyPixel(pixel,3,0))       # behind head diff color
+        and
+        Color(image,pixel) != Color(image,NearbyPixel(pixel,3,3))       # behind leg diff color
+    ):
+        return "AMOGUS"
+    else:
+        return "NOPE"
 
-
-
+def CheckSmolAmogusRight(image, pixel):
+    if(
+        Color(image,NearbyPixel(pixel,1,0)) == 
+        Color(image,NearbyPixel(pixel,2,0)) == 
+        Color(image,NearbyPixel(pixel,3,0)) ==
+        Color(image,NearbyPixel(pixel,0,1)) ==
+        Color(image,NearbyPixel(pixel,1,1)) ==
+        Color(image,NearbyPixel(pixel,0,2)) ==
+        Color(image,NearbyPixel(pixel,1,2)) ==
+        Color(image,NearbyPixel(pixel,2,2)) ==
+        Color(image,NearbyPixel(pixel,3,2)) ==
+        Color(image,NearbyPixel(pixel,1,3)) ==
+        Color(image,NearbyPixel(pixel,3,3))
+        and
+        Color(image,NearbyPixel(pixel,2,1)) == Color(image,NearbyPixel(pixel,3,1))  # eyes same color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,2,1))       # eyes diff color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,2,3))       # between legs diff color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,0,0))       # behind head diff color
+        and
+        Color(image,NearbyPixel(pixel,1,0)) != Color(image,NearbyPixel(pixel,0,3))       # behind leg diff color
+    ):
+        return "AMOGUS"
+    else:
+        return "NOPE"
 
 #imageToParse = '10x10_test.png'
-imageToParse = '100x100_piece.png'
+#imageToParse = '100x100_piece.png'
+imageToParse = 'place_FULL.png'
 
 file_name = os.path.join(os.path.dirname(__file__), imageToParse)
 assert os.path.exists(file_name)
@@ -56,13 +141,29 @@ source = cv2.imread(file_name)
 
 height, width, _ = source.shape
 
+count = 0
+
 for y in range(0,height-4):
     for x in range(0, width-3):
         pixel = (x,y)
         if CheckAmogusLeft(source,pixel) == "AMOGUS":
-            print(pixel)
+            print("Left ", pixel)
+            count += 1
+        elif CheckAmogusRight(source,pixel) == "AMOGUS":
+            print("Right ", pixel) 
+            count += 1
 
+for y in range(0,height-3):
+    for x in range(0, width-3):
+        pixel = (x,y)
+        if CheckSmolAmogusLeft(source,pixel) == "AMOGUS":
+            print("Smol Left ", pixel)
+            count += 1
+        elif CheckSmolAmogusRight(source,pixel) == "AMOGUS":
+            print("Smol Right ", pixel)
+            count += 1
 
+print("Total Amogi: ", count)
 
 
  
